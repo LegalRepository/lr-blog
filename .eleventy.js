9d3ac4d96e2f401e076254d67a04374e7d13dc2a
Srivats Shankar <srivats@aipolicy.xyz>
2019-05-03 13:06:12 +0530

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dir: {
+    input: "input", // The directory responsible for handling all content
+    output: "public", // The directory for all output
+    includes: "_includes", // The directory for including any components with the template
+    layouts: "_layouts", // The directory for layouts
+    data: "_global" // The directly for global data files
+  }
+};
